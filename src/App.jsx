1a1e714ed273d15703937eb494a0ab5a5f53356d
kasimurkmez <kasimurkmez47@gmail.com>
2023-07-09 02:33:11 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,56 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FormTodo from "./FormTodo";
+import { useState } from "react";
+import { Card } from "react-bootstrap";
+import Todo from "./Todo";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "This is a sampe todo",
+      isDone: false,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const markTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <>
-      <h2>Hello</h2>
-    </>
+    <div className="App">
+      <div className="container">
+        <h1 className="text-center text-danger ">Todo List</h1>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, index) => (
+            <Card key={index} className="mt-4">
+              <Card.Body>
+                <Todo
+                  index={index}
+                  todo={todo}
+                  markTodo={markTodo}
+                  removeTodo={removeTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/FormTodo.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const FormTodo = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Label>
+          <b>Add Todo</b>
+        </Form.Label>
+        <Form.Control
+          type="text"
+          className="input"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Add new todo"
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" className="btn btn-primary mt-3">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+export default FormTodo;
